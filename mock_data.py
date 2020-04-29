3fb12b84d57fce099ad365ae6b6ba50ba4de1775
Shaun Wu <shaunwu25@gmail.com>
2020-04-29 20:21:41 +0800

    develop mock_data.py for updating data to database

--- mock_data.py
+++ mock_data.py
@@ -1,6 +1,8 @@
 from models import *
 from app import db, app
 import sys
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.dialects.mysql import insert
 
 venues_data=[{
     "city": "San Francisco",
@@ -23,11 +25,10 @@ venues_data=[{
       "num_upcoming_shows": 0,
     }]
 }]
-
-def create_venues_data(venues_data):
+def create_venues_data(datas):
     try:
         error = False
-        for data in venues_data:
+        for data in datas:
             city = data['city']
             state = data["state"]
             for venue in data['venues']:
@@ -45,5 +46,137 @@ def create_venues_data(venues_data):
     finally:
         db.session.close()
 
+#------------------------------------------------------------------------------------------------------
+data1={
+    "id": 1,
+    "name": "The Musical Hop",
+    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+    "address": "1015 Folsom Street",
+    "city": "San Francisco",
+    "state": "CA",
+    "phone": "123-123-1234",
+    "website": "https://www.themusicalhop.com",
+    "facebook_link": "https://www.facebook.com/TheMusicalHop",
+    "seeking_talent": True,
+    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    "past_shows": [{
+      "artist_id": 4,
+      "artist_name": "Guns N Petals",
+      "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+      "start_time": "2019-05-21T21:30:00.000Z"
+    }],
+    "upcoming_shows": [],
+    "past_shows_count": 1,
+    "upcoming_shows_count": 0,
+}
 
+data2={
+"id": 2,
+"name": "The Dueling Pianos Bar",
+"genres": ["Classical", "R&B", "Hip-Hop"],
+"address": "335 Delancey Street",
+"city": "New York",
+"state": "NY",
+"phone": "914-003-1132",
+"website": "https://www.theduelingpianos.com",
+"facebook_link": "https://www.facebook.com/theduelingpianos",
+"seeking_talent": False,
+"image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+"past_shows": [],
+"upcoming_shows": [],
+"past_shows_count": 0,
+"upcoming_shows_count": 0,
+}
+data3={
+"id": 3,
+"name": "Park Square Live Music & Coffee",
+"genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
+"address": "34 Whiskey Moore Ave",
+"city": "San Francisco",
+"state": "CA",
+"phone": "415-000-1234",
+"website": "https://www.parksquarelivemusicandcoffee.com",
+"facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
+"seeking_talent": False,
+"image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+"past_shows": [{
+    "artist_id": 5,
+    "artist_name": "Matt Quevedo",
+    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    "start_time": "2019-06-15T23:00:00.000Z"
+}],
+"upcoming_shows": [{
+    "artist_id": 6,
+    "artist_name": "The Wild Sax Band",
+    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+    "start_time": "2035-04-01T20:00:00.000Z"
+}, {
+    "artist_id": 6,
+    "artist_name": "The Wild Sax Band",
+    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+    "start_time": "2035-04-08T20:00:00.000Z"
+}, {
+    "artist_id": 6,
+    "artist_name": "The Wild Sax Band",
+    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+    "start_time": "2035-04-15T20:00:00.000Z"
+}],
+"past_shows_count": 1,
+"upcoming_shows_count": 1,
+}
+
+show_venue_datas = [data1, data2, data3]
+
+def create_show_venue_datas(datas):
+    try:
+        error = False
+        for data in datas:
+            try:
+                error02 = False
+
+                id = data['id']
+                genres = data['genres']
+                address = data["address"]
+                phone = data["phone"]
+                website = data["website"]
+                facebook_link = data["facebook_link"]
+                seeking_talent = data["seeking_talent"]
+                image_link = data["image_link"]
+
+                if "seeking_description" in data:
+                    seeking_description = data["seeking_description"]
+                    print("seeking_description existed")
+                else:
+                    seeking_description = 'NA'
+                
+                row_data = Venue.query.filter(Venue.id == id).one()
+                row_data.genres = genres
+                row_data.address = address
+                row_data.phone = phone
+                row_data.website = website
+                row_data.facebook_link = facebook_link
+                row_data.seeking_talent = seeking_talent
+                row_data.image_link = image_link
+                row_data.seeking_description = seeking_description
+
+                db.session.commit()
+            except:
+                error02 = True
+                db.session.rollback()
+                print("in" + sys.exc_info())
+            
+            finally:
+                db.session.close()
+    except:
+        error = True
+        db.session.rollback()
+        print("out" + sys.exc_info())
+    
+    finally:
+        db.session.close()
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
 create_venues_data(venues_data)
+create_show_venue_datas(show_venue_datas)
